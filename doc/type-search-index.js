55bb5e90568d945896da2ee769701fd151ead320
Johan Jojoa <johanpipej75@gmail.com>
2021-12-07 12:00:37 -0500

    Final

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"model","l":"Brand"},{"p":"model","l":"Company"},{"p":"ui","l":"DataCenter"},{"p":"model","l":"DataCenterSystem"},{"p":"model","l":"Disk"},{"p":"model","l":"MiniRoom"},{"p":"model","l":"Processor"},{"p":"model","l":"Project"},{"p":"model","l":"Server"},{"p":"model","l":"User"}]
